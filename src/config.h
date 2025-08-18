1d835693a92aeacc481a572d6474cdf454869f5c
Chris87-ux <55442459+Chris87-ux@users.noreply.github.com>
2025-08-18 12:24:26 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -32,53 +32,53 @@
 
 // -- LDR (example) --
 #define LDR_PIN 34
-#define LDR_TOPIC "weatherstation/livingroom/ldr"
+#define LDR_TOPIC "weatherstation/ldr"
 
 // -- MQ-2 Gas Sensor --
 #define MQ2_GAS_PIN 32
-#define MQ2_TOPIC "weatherstation/livingroom/gas"
+#define MQ2_TOPIC "weatherstation/gas"
 
 // -- GY-8511 UV Sensor --
 #define GY8511_UV_PIN 33
-#define UV_TOPIC "weatherstation/livingroom/uv"
+#define UV_TOPIC "weatherstation/uv"
 
 // -- Wind Direction Sensor (WH-SP-WD) --
 #define WIND_DIRECTION_PIN 35
-#define WIND_DIR_TOPIC "weatherstation/livingroom/winddir"
+#define WIND_DIR_TOPIC "weatherstation/winddir"
 
 // -- Rain Gauge (MS-WH-SP-RG) --
 #define RAIN_GAUGE_PIN 26
-#define RAIN_GAUGE_TOPIC "weatherstation/livingroom/rain"
+#define RAIN_GAUGE_TOPIC "weatherstation/rain"
 #define RAIN_MM_PER_PULSE 0.2794 // Amount of rain per pulse/tip
 
 // -- Wind Speed Sensor (WH-SP-WS01) --
 #define WIND_SPEED_PIN 27
-#define WIND_SPEED_TOPIC "weatherstation/livingroom/windspeed"
+#define WIND_SPEED_TOPIC "weatherstation/windspeed"
 #define WIND_KMH_PER_PULSE_PER_SEC 2.4 // Factor to convert (pulses/sec) to km/h
 
 // -- BH1750 Light Sensor --
 #define BH1750_I2C_ADDRESS 0x23
-#define BH1750_TOPIC "weatherstation/livingroom/light"
+#define BH1750_TOPIC "weatherstation/light"
 
 // -- AS3935 Lightning Sensor --
 #define AS3935_I2C_ADDRESS 0x03
 #define AS3935_IRQ_PIN 4
-#define AS3935_TOPIC "weatherstation/livingroom/lightning"
+#define AS3935_TOPIC "weatherstation/lightning"
 
 // -- MH-Z19E CO2 Sensor --
 #define MHZ19_RX_PIN 16
 #define MHZ19_TX_PIN 17
-#define MHZ19_TOPIC "weatherstation/livingroom/co2"
+#define MHZ19_TOPIC "weatherstation/co2"
 
 
 // -- SGP30 Air Quality Sensor --
 #define SGP30_I2C_ADDRESS 0x58
-#define SGP30_TOPIC "weatherstation/livingroom/airquality"
+#define SGP30_TOPIC "weatherstation/airquality"
 
 // -- PMS5003 Air Quality Sensor --
 #define PMS5003_RX_PIN 18
 #define PMS5003_TX_PIN 19
-#define PMS5003_TOPIC "weatherstation/livingroom/particles"
+#define PMS5003_TOPIC "weatherstation/particles"
 
 // -- Soil Moisture & Temp Sensor --
 #define SOIL_MOISTURE_PIN 36 // ADC1_CH0
